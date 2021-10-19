51201d24908610b959ce7db25c98cc8818f88efe
ZYEYT <71366015+ZYEYT@users.noreply.github.com>
2021-10-19 13:03:47 +0800

    Add files via upload

--- srcipt.js
+++ srcipt.js
@@ -1,15 +1,20 @@
-const hexNumbers =[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+const hexNumbers =[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 const hexBtn=document.querySelector('.hexBtn');
 const bodyBcg =document.querySelector('body');
 const hex =document.querySelector('.hex');
-hexBtn.addEventListner('click',getHex);
+
+hexBtn.addEventListener('click',getHex);
 
 
-function getHex='#';
+function getHex(){
 let hexCol='#';
-for(let 1=-;1<6;1++)
+
+for(let i=0;i<6;i++){
+let random = Math.floor(Math.random()*hexNumbers.length);
+//console.log(random);
 hexCol +=hexNumbers[random];
+//console.log(hexCol);
 }
-bodyBcg.style.backgroundcolor=hexCol;
-hexCol.innerHTML=hexCol;
+bodyBcg.style.backgroundColor = hexCol;
+hex.innerHTML = hexCol; 
 }
